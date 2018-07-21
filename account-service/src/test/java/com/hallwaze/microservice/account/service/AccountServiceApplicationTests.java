bdbb0b23a45da590ca08aacc98a9a83c91c27b7e
Brijesh Singh <brajput24@gmail.com>
2018-07-20 21:34:41 -0400

    Initial commit

---
+++ account-service/src/test/java/com/hallwaze/microservice/account/service/AccountServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.hallwaze.microservice.account.service;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Ignore
+public class AccountServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
